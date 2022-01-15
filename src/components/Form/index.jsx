5ba1706a6f299a44fee5f8406041cc35dcf63f9c
ThalesTrombim <thalestr@outlook.com>
2022-01-14 23:49:50 -0300

    Final do curso

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,17 +2,30 @@ import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel, Typography } from '@material-ui/core';
 
 function Form() {
-    const [ nome, setNome ] = useState('');
+    const [ user, setUser ] = useState({});
+    const [ errors, setErrors ] = useState({ cpf:{ valid:true, text: ''} })
+    const [ promotions, setPromotions ] = useState(true)
+    const [ newsletter, setNewsletter ] = useState(false)
+
+    function validatedCpf(cpf){
+        if(cpf.length !== 11){
+            return { valid: false, text: 'Cpf inválido'}
+        } else {
+            return { valid: true, text: ''}
+        }
+    }
 
     return (
         <form onSubmit={(e) => {
             e.preventDefault();
+            console.log(user)
         }}>
             <Typography variant='h3' component='h1' align='center'>Formulário de cadastro</Typography>
 
             <TextField
                 onChange={(e) => {
-                    setNome(e.target.value)
+                    user.name = e.target.value
+                    setUser(user)
                 }}
                 variant='outlined'
                 color='secondary'
@@ -22,6 +35,10 @@ function Form() {
             />
 
             <TextField 
+                onChange={(e) => {
+                    user.sobrenome = e.target.value
+                    setUser(user)
+                }}
                 variant='outlined' 
                 color='secondary' 
                 fullWidth 
@@ -31,6 +48,14 @@ function Form() {
             />
 
             <TextField 
+                onBlur={(e) => {
+                    const validCpf = validatedCpf(e.target.value);
+                    setErrors({ cpf: validCpf })
+                    user.cpf = e.target.value
+                    setUser(user)
+                }}
+                error={!errors.cpf.valid}
+                helperText={errors.cpf.text}
                 variant='outlined' 
                 color='secondary' 
                 fullWidth 
@@ -40,13 +65,27 @@ function Form() {
 
             <FormControlLabel label='Promoções' control={
                 <Switch
-                    name='promocoes' defaultChecked color='secondary'
+                    checked={promotions}
+                    onChange={(e) => {
+                        setPromotions(e.target.checked)
+                        user.promotions = e.target.checked
+                        setUser(user)
+                    }}
+                    name='promocoes' 
+                    color='secondary'
                 />
             } />
 
             <FormControlLabel label='Novidades' control={
                 <Switch
-                    name='novidades' defaultChecked color='primary'
+                    checked={newsletter}
+                    onChange={(e) => {
+                        setNewsletter(e.target.checked)
+                        user.newsletter = e.target.checked
+                        setUser(user)
+                    }}
+                    name='novidades' 
+                    color='primary'
                 />
             } />
 
